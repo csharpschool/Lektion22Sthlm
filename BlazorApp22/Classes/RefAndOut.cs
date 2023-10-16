65a7101edd9b57ab939e5949675806ae840a668e
Jonas Fagerberg <info@csharpschool.com>
2023-10-16 11:19:05 +0200

    Ref and Out

---
+++ BlazorApp22/Classes/RefAndOut.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp22.Classes;
+
+public class RefAndOut
+{
+    public int Calculate(ref int x, int y, out int z)
+    {
+        var result = x + y;
+
+        x = result * 2;
+        y = x * 10;
+        z = y * 100;
+        return result;
+    }
+}
